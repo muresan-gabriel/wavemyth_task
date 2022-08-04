679b2e9051fb43245027053dec334b496e67640e
muresan-gabriel <gabiimuresan23@gmail.com>
2022-08-04 13:45:43 +0300

    Slider Basic Container Setup

---
+++ src/components/Checkout.js
@@ -0,0 +1,11 @@
+import Slider from "./Slider";
+
+const Checkout = () => {
+  return (
+    <div className="checkout-container col-8">
+      <Slider></Slider>
+    </div>
+  );
+};
+
+export default Checkout;
